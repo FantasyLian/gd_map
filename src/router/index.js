40efb596c09afded88b5354a80f4ced0c53015c5
Mr.cat <fantasy.lian@163.com>
2022-07-14 11:08:12 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/MapContainer.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/MapContainer',
     name: 'mapContainer',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "mapContainer" */ '../views/MapContainer.vue')
+    component: HomeView
   }
 ]
 
