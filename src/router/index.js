ab77bbae9e45c657ac69ef86e39028e42ff92ef4
Mr.cat <fantasy.lian@163.com>
2022-07-19 17:05:34 +0800

    增加路由

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,20 @@ const routes = [
     path: '/',
     name: 'mapContainer',
     component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
+  },
+  {
+    path: '/404',
+    name: 'ErrorView',
+    component: () => import(/* webpackChunkName: "404" */ '@/views/ErrorView.vue')
+  },
+  {
+    path: '/:catchAll(.*)', // 不识别的path自动匹配404
+    redirect: '404'
   }
 ]
 
