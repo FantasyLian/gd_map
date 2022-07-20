24efdb5bc86781153e1aff857b11fac990ff36fa
Mr.cat <fantasy.lian@163.com>
2022-07-20 15:41:01 +0800

    加入antd框架並按需引入

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,41 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = '羅甸縣公安定位'
+      return args
+    })
+  },
+
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          modifyVars: {
+            'primary-color': '#3778e7',
+            'link-color': '#3778e7',
+            'border-radius-base': '2px'
+          },
+          javascriptEnabled: true
+        }
+      }
+    }
+  },
+
+  devServer: {
+    open: true,
+    host: '0.0.0.0'
+    // proxy: {
+    //   '/api': {
+    //     target: '',
+    //     ws: true,
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       '^/api': ''
+    //     }
+    //   }
+    // }
+  }
 })
