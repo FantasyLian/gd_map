60b17a401447906677fc6746d8bb1bc416c59be7
Mr.cat <fantasy.lian@163.com>
2022-07-21 10:55:46 +0800

    登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import LayoutView from '../views/LayoutView.vue'
+import local from '@/utils/local'
+import LayoutView from '@/views/LayoutView.vue'
 
 const routes = [
   {
@@ -14,18 +15,47 @@ const routes = [
     }]
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'Login',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (login.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
+  },
+  {
+    path: '/404',
+    name: 'NoteFound',
+    component: () => import(/* webpackChunkName: "404" */ '@/views/NotFound.vue')
+  },
+  {
+    path: '/500',
+    name: 'ServerError',
+    component: () => import(/* webpackChunkName: "500" */ '@/views/ServerError')
+  }, {
+    path: '/:catchAll(.*)', // 不识别的path自动匹配404
+    redirect: '404'
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const LOGIN_URL = '/login'
+  const url = to.path
+  const user = local.get('token')
+
+  if (url === LOGIN_URL) {
+    local.remove('token')
+  }
+
+  if (!user && url !== LOGIN_URL) {
+    next({ path: LOGIN_URL })
+  } else {
+    next()
+  }
 })
 
 export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,151 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="login-wrap">
+        <img src="../assets/images/icon_police_badge.png" alt="logo" />
+        <h1>罗甸县公安局警力分布系统</h1>
+        <a-form
+          :model="formState"
+          autocomplete="off"
+          @finish="onFinish"
+          @finishFailed="onFinishFailed"
+        >
+          <a-form-item
+            name="username"
+            :rules="[{ required: true, message: '请输入账号！' }]"
+          >
+            <a-input
+              v-model:value="formState.username"
+              placeholder="请输入账号"
+            >
+              <template #prefix
+                ><UserOutlined style="color: rgba(0, 0, 0, 0.25)"
+              /></template>
+            </a-input>
+          </a-form-item>
+
+          <a-form-item
+            name="password"
+            :rules="[{ required: true, message: '请输入账号！' }]"
+          >
+            <a-input-password
+              v-model:value="formState.password"
+              placeholder="请输入密码"
+            >
+              <template #prefix
+                ><LockOutlined style="color: rgba(0, 0, 0, 0.25)"
+              /></template>
+            </a-input-password>
+          </a-form-item>
+
+          <a-form-item>
+            <a-button
+              type="primary"
+              html-type="submit"
+              block
+              :disabled="formState.user === '' || formState.password === ''"
+              >登录</a-button
+            >
+          </a-form-item>
+        </a-form>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import { defineComponent, reactive } from 'vue'
+import { Form, Button, Input } from 'ant-design-vue'
+import { UserOutlined, LockOutlined } from '@ant-design/icons-vue'
+export default defineComponent({
+  components: {
+    AForm: Form,
+    AFormItem: Form.Item,
+    AInput: Input,
+    AInputPassword: Input.Password,
+    AButton: Button,
+    UserOutlined,
+    LockOutlined
+  },
+  setup () {
+    const formState = reactive({
+      username: '',
+      password: ''
+    })
+
+    const onFinish = values => {
+      console.log('Success:', values)
+    }
+
+    const onFinishFailed = errorInfo => {
+      console.log('Failed:', errorInfo)
+    }
+
+    return {
+      formState,
+      onFinish,
+      onFinishFailed
+    }
+  }
+})
+</script>
+<style scoped lang="less">
+section {
+  height: 100vh;
+  background: #d8d8d8 linear-gradient(180deg, #161e86 0%, #131724 100%);
+}
+.container {
+  width: 1500px;
+  height: 100%;
+  margin: 0 auto;
+  background: url("@/assets/images/login_bg.png") no-repeat left center, linear-gradient(180deg, #161e86 0%, #131724 100%);
+  display: flex;
+  justify-content: end;
+  align-items: center;
+}
+.login-wrap {
+  width: 430px;
+  padding: 78px 45px;
+  background-color: #fff;
+  border-radius: 10px;
+  img {
+    width: 70px;
+    height: 70px;
+  }
+  h1 {
+    height: 40px;
+    font-size: 28px;
+    font-weight: 500;
+    color: #353535;
+    line-height: 40px;
+    margin: 30px auto;
+  }
+  .ant-form {
+    .ant-form-item {
+      &:last-child {
+        margin-bottom: 0;
+      }
+      .ant-input-affix-wrapper {
+        font-size: 20px;
+      }
+    }
+    :deep(.ant-input) {
+      width: 340px;
+      height: 40px;
+      font-size: 16px;
+      font-weight: 400;
+      color: #b2b2b2;
+      line-height: 40px;
+    }
+    .ant-btn {
+      width: 340px;
+      height: 50px;
+      // background-color: #145bf8;
+      border-radius: 4px;
+      font-size: 18px;
+      font-weight: 500;
+      color: #fff;
+      line-height: 25px;
+    }
+  }
+}
+</style>
