2ebf9e81d9baf9bb004e53b540bda5b03b7f64dd
Mr.cat <fantasy.lian@163.com>
2022-07-21 10:10:47 +0800

    增加错误提示页面

---
+++ src/components/Error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="error">Sorry, {{ msg }} :)</div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorInfo',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.error {
+  font-size: 26px;
+  letter-spacing: 5px;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
---
+++ src/views/ServerError.vue
@@ -0,0 +1,15 @@
+<template>
+  <Error msg="Server Error" />
+</template>
+
+<script>
+// @ is an alias to /src
+import Error from '@/components/Error.vue'
+
+export default {
+  name: 'serverError',
+  components: {
+    Error
+  }
+}
+</script>
